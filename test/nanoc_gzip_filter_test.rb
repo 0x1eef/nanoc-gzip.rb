6ab0a45bf3e7c6b2ae2b2ad0610ae77767c60ea1
0x1eef <0x1eef@protonmail.com>
2023-06-29 17:28:34 -0300

    Add tests

---
+++ test/nanoc_gzip_filter_test.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+require_relative "setup"
+
+class FilterTest < Test::Unit::TestCase
+  module BinaryItem
+    def item
+      OpenStruct.new("binary?" => true)
+    end
+  end
+
+  module TextItem
+    def item
+      OpenStruct.new("binary?" => false)
+    end
+  end
+
+  def teardown
+    FileUtils.rm(filter.output_filename)
+  end
+
+  def test_text_item
+    filter.extend(TextItem).run("hello world")
+    assert_equal "application/x-gzip", file(filter.output_filename)
+  end
+
+  def test_binary_item
+    bin_file = File.join(__dir__, "bin_test_file")
+    File.binwrite(bin_file, "hello world")
+    filter.extend(BinaryItem).run(bin_file)
+    assert_equal "application/x-gzip", file(filter.output_filename)
+  ensure
+    FileUtils.rm(bin_file)
+  end
+
+  private
+
+  def filter
+    @filter ||= Nanoc::Filter.named!(:gzip).new
+  end
+
+  def file(path)
+    %x(file -bi #{path}).chomp
+  end
+end
